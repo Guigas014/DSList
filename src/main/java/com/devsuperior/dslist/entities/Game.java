e9b56ad035b4f92f8fefac51991f0058a0c7065e
Guilherme Alves Araujo <guilherme-allves@hotmail.com>
2023-07-13 00:10:35 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -23,21 +23,27 @@ public class Game {
     private Integer year;
     
     private String genre;
-    private String platform;
+    private Double score;
+    private String platforms;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game () {}
 
-    public Game (Long id, String title, Integer year, String genre, String platform, 
-    String imgUrl, String shortDescription, String longDescription) {
+    public Game (Long id, String title, Integer year, String genre, Double score, 
+    String platforms, String imgUrl, String shortDescription, String longDescription) {
         super();
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.score = score;
+        this.platforms = platforms;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -67,11 +73,17 @@ public class Game {
     public void setGenre(String genre) {
         this.genre = genre;
     }
-    public String getPlatform() {
-        return platform;
+    public Double getScore() {
+        return score;
     }
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setScore(Double score) {
+        this.score = score;
+    }
+    public String getPlatforms() {
+        return platforms;
+    }
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
     public String getImgUrl() {
         return imgUrl;
